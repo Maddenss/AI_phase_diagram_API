91464be6d5d58c9548c29f1e4f52978262757651
MikhailRogovoy <47089292+MikhailRogovoy@users.noreply.github.com>
2024-12-31 02:39:20 +0500

    Update test_load.py

--- test_load.py
+++ test_load.py
@@ -8,6 +8,7 @@ from statistics import mean
 # Нагрузочный тест для проверки стабильности системы и расчета времени отклика
 client = TestClient(app)
 
+
 def test_stability_with_multiple_requests():
     """
     Проверка стабильности системы при многократных запросах.
@@ -20,10 +21,9 @@ def test_stability_with_multiple_requests():
         finish = time.thread_time()
         time_response = finish-start
         time_list.append(time_response)
-        assert response.status_code == 200
-        
+        assert response.status_code == 200        
     avg_time = mean(time_list)
 
     # Выводим информацию о тесте
     print("\nНагрузочный тест: система стабильна при многократных запросах.")
-    print("Среднее время отклика: " + str(avg_time))    
+    print("Среднее время отклика: " + str(avg_time))
