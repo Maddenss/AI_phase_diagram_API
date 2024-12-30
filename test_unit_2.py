12fab6561f8b00e6e16db463e396c2782b559c75
MikhailRogovoy <47089292+MikhailRogovoy@users.noreply.github.com>
2024-12-31 02:45:08 +0500

    Update test_unit_2.py

--- test_unit_2.py
+++ test_unit_2.py
@@ -15,18 +15,18 @@ def test_load_data(loaded_data):
     Проверка корректности загрузки данных.
     """
     X, y = loaded_data
-        # Проверяем, что X — это DataFrame, а y — Series
+    # Проверяем, что X — это DataFrame, а y — Series
     assert isinstance(X, pd.DataFrame)
     assert isinstance(y, pd.Series)
-        # Проверяем, что данные не пустые
+    # Проверяем, что данные не пустые
     assert not X.empty
     assert not y.empty
-        # Проверяем, что в X есть ожидаемые колонки
+    # Проверяем, что в X есть ожидаемые колонки
     expected_columns = ["T", "E", "C", "FM", "Xfm", "AFM", "Xafm"]
     assert all(col in X.columns for col in expected_columns)
-        # Проверяем, что y содержит только бинарные значения (0 и 1)
+    # Проверяем, что y содержит только бинарные значения (0 и 1)
     assert set(y.unique()).issubset({0, 1})
-        # Выводим информацию о данных
+    # Выводим информацию о данных
     print("\nМодульный тест 2.")
     print("Данные успешно загружены:")
     print(f"X (признаки): {X.shape[0]} строк, {X.shape[1]} столбцов")
